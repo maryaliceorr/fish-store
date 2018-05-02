93ad5a7e9344fcd72c057e6e678de51dc3c26211
Mary Alice Orr <m.a.orr17@gmail.com>
2018-05-01 20:51:06 -0500

    added discounting

---
+++ javascripts/discount.js
@@ -0,0 +1,15 @@
+const discount = 0.2;
+
+const applySale = () => {
+  $('.on-sale').each((i, fishCard) => {
+    const basePriceElement = $(fishCard).find('.price');
+    const basePrice = basePriceElement.html() * 1;
+    // this will come back as text so have to add * 1
+    const newPrice = (basePrice * (1 - discount)).toFixed(2);
+    // creating the discounted price - the toFixed sets the decimal places and the 2 is how many decimals
+    basePriceElement.html(newPrice);
+    // want this to take the place of the old price
+  });
+};
+
+module.exports = applySale;
